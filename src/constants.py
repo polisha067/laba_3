dd8203b20b2fd2f037e3816fe434f411fc418ffe
polisha <polinazuravlik2@gmail.com>
2025-12-13 16:33:22 +0300

    update main

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+SUPPORTED_SORT_ALGORITHMS = ['bubble', 'quick', 'counting', 'heap', 'radix', 'bucket']
+
+ERROR_EMPTY_ARRAY = "массив не может быть пустым"
+ERROR_NEGATIVE_NUMBER = "число не может быть отрицательным"
+ERROR_STACK_EMPTY = "стек пустой"
+ERROR_QUEUE_EMPTY = "очередь пустая"
+
+RADIX_BASE = 10
